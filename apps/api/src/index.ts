f25f4c27225c3d0917f36ec373afb1a635527568
Gilmar Andrade <gilmar-andrade@outlook.com>
2025-08-31 17:37:12 -0300

    chores [api]: configure eslint

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,12 +1,12 @@
-import { formatFileSize } from "@utils/formatter";
+import { formatFileSize } from "@utils/formatter"
 
 
 if (process.argv.length > 2) {
-  const sizeBytes = parseInt(process.argv[2]);
-  const formattedSize = formatFileSize(sizeBytes);
-  console.log(formattedSize);
+  const sizeBytes = parseInt(process.argv[2])
+  const formattedSize = formatFileSize(sizeBytes)
+  console.log(formattedSize)
 } else {
   console.log(
     'Please provide the file size in bytes as a command-line argument.'
-  );
+  )
 }
--- apps/api/tests/formatter.test.ts
+++ apps/api/tests/formatter.test.ts
@@ -1,9 +1,9 @@
-import { describe, it } from "node:test";
-import assert from "node:assert";
-import { formatFileSize } from "@utils/formatter";
+import { describe, it } from "node:test"
+import assert from "node:assert"
+import { formatFileSize } from "@utils/formatter"
 
 describe("formatFileSize function", () => {
   it('should return "1.00 GB" for sizeBytes = 1073741824', () => {
-    assert.strictEqual(formatFileSize(1073741824), "1.00 GB");
-  });
-});
+    assert.strictEqual(formatFileSize(1073741824), "1.00 GB")
+  })
+})
