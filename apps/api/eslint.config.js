0e0df6553bf3e74ec900e17f73702bf32bde820d
Gilmar Andrade <gilmar-andrade@outlook.com>
2025-08-31 18:12:53 -0300

    chores [api]: configure prettier code formatter

--- apps/api/eslint.config.js
+++ apps/api/eslint.config.js
@@ -1,20 +1,20 @@
 // @ts-check
 
-import js from "@eslint/js"
-import globals from "globals"
-import tseslint from "typescript-eslint"
-import { defineConfig } from "eslint/config"
-import prettierConfig from 'eslint-config-prettier';
+import js from '@eslint/js'
+import globals from 'globals'
+import tseslint from 'typescript-eslint'
+import { defineConfig } from 'eslint/config'
+import prettierConfig from 'eslint-config-prettier'
 
 export default defineConfig([
     {
-        files: ["**/*.{js,mjs,cjs,ts,mts,cts}"],
+        files: ['**/*.{js,mjs,cjs,ts,mts,cts}'],
         plugins: { js },
-        extends: ["js/recommended"],
+        extends: ['js/recommended'],
         languageOptions: { globals: globals.node },
         rules: {
-            semi: ["error", "never"],
-        }
+            semi: ['error', 'never'],
+        },
     },
     tseslint.configs.recommended,
     prettierConfig,
