f45c4825a7be1f321d16ed50da12525948795416
Gilmar Andrade <gilmar-andrade@outlook.com>
2025-10-07 22:22:55 -0300

    refactor: RegisterUserController handles MissinParamError

--- apps/api/src/iam/infra/controllers/RegisterUserController.ts
+++ apps/api/src/iam/infra/controllers/RegisterUserController.ts
@@ -5,31 +5,38 @@ import { DomainError } from "@utils/DomainError"
 import { MissingParamError } from "@utils/MissingParamError"
 
 export class RegisterUserController {
+    private readonly requiredParamNames = ['name', 'email', 'password']
+
     constructor(private readonly usecase: RegisterUserUseCase) {}
+
+    private validateRequiredParams(request: HttpRequest) {
+        const missingParams = this.requiredParamNames.filter(paramName => {
+            return (!request.body[paramName]) ? true : false
+        })
+
+        if(missingParams.length > 0) {
+            throw new MissingParamError(`Missing parameters from request: ${missingParams.join(', ')}.`)
+        }
+
+        return true
+    }
 
     public async handle(request: HttpRequest): Promise<HttpResponse> {
         try {
-            const requiredParamNames = ['name', 'email', 'password']
-
-            const missingParams = requiredParamNames.filter(paramName => {
-                return (!request.body[paramName]) ? true : false
-            })
-
-            if(missingParams.length > 0) {
-                return badRequest(new MissingParamError(`Missing parameters from request: ${missingParams.join(', ')}.`))
+            if(this.validateRequiredParams(request)) {
+                const command = {
+                    email: request.body.email,
+                    name: request.body.name,
+                    password: request.body.password
+                }
+            
+                const response = await this.usecase.execute(command)
+                return created(response)
             }
-
-            const command = {
-                email: request.body.email,
-                name: request.body.name,
-                password: request.body.password
-            }
-        
-            const response = await this.usecase.execute(command)
-            return created(response)
-
         } catch(error: unknown) {
             if(error instanceof DomainError) {
+                return badRequest(error)
+            } else if(error instanceof MissingParamError) {
                 return badRequest(error)
             }
 
